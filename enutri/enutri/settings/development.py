d52c4f2b8b7b92f4a5bbe0666423b7d270872bb4
Casa <casa@iMac-de-Casa.local>
2019-12-15 18:10:12 +0100

    Initial commit

---
+++ enutri/enutri/settings/development.py
@@ -0,0 +1,23 @@
+from .base import *
+
+INSTALLED_APPS += [
+    'debug_toolbar', 
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+INTERNAL_IP = [
+    '127.0.0.1',
+]
